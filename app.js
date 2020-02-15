69538466585e2aa86d269c998d2dbf2e731abbe9
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-16 01:09:01 +0500

    express app created

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { join } from 'path';
+import { address } from 'ip';
+import { createServer } from 'http';
+import cors from 'cors';
+import server from './src';
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.disable('x-powered-by');
+
+app.use(express.static(join(__dirname, '../jurnal_client/build')));
+
+const http = createServer(app, cors());
+
+server.applyMiddleware({ app });
+
+app.get('*', (_, res) => res.sendFile(join(__dirname, '../jurnal_client/build/index.html')));
+
+http.listen({ port }, () => {
+  // eslint-disable-next-line no-console
+  console.log('\x1b[36m', `Local:    http://localhost:${port}`);
+  // eslint-disable-next-line no-console
+  console.log('\x1b[36m', `Neetwork: http://${address()}:${port}`, '\x1b[0m');
+});
