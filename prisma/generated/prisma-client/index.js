c80863020288976ba07a130224930fa37fd498a8
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-16 01:12:28 +0500

    prisma role seeder added

---
+++ prisma/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+
+Object.defineProperty(exports, '__esModule', { value: true });
+const prisma_lib_1 = require('prisma-client-lib');
+const { typeDefs } = require('./prisma-schema');
+
+const models = [
+  {
+    name: 'User',
+    embedded: false,
+  },
+  {
+    name: 'Role',
+    embedded: false,
+  },
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: 'http://localhost:4466',
+});
+exports.prisma = new exports.Prisma();
---
+++ prisma/seeders/roles.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'admin',
+  },
+  {
+    id: 2,
+    name: 'author',
+  },
+  {
+    id: 3,
+    name: 'editor',
+  },
+];
