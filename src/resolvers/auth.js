2d89cd81403ea698881f7d18b05033375473ca0c
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-23 10:34:07 +0500

    Auth resolver modified

--- src/resolvers/auth.js
+++ src/resolvers/auth.js
@@ -1,56 +1,43 @@
 import { sign } from 'jsonwebtoken';
-import { compareSync, hashSync } from 'bcryptjs';
+import { compareSync } from 'bcryptjs';
 import { AuthenticationError } from 'apollo-server-express';
-
-const fragment = `
-fragment UserWithRole on User {
-    id
-    name
-    username
-    password
-    role {
-        id
-        name
-    }
-}
-`;
+import { FRAGMENT_USER } from '../fragments';
 
 export default {
   Query: {
     details(parent, arg, { prisma, auth }, info) {
-      const { userId } = auth();
-      return prisma.user({ id: userId }, info).$fragment(fragment);
+      const userId = auth();
+      return prisma.user({ id: userId }, info).$fragment(FRAGMENT_USER);
     },
   },
   Mutation: {
     async signIn(parent, { data }, { prisma }, info) {
-      const { username, password } = data;
+      const { userName, password } = data;
       try {
-        const user = await prisma.user({ username }).$fragment(fragment);
+        const user = await prisma.user({ userName }).$fragment(FRAGMENT_USER);
+
+        if (!user) throw new AuthenticationError('Username or password incorrect');
+        if (user.blocked) throw new AuthenticationError('You are blocked');
+
         if (compareSync(password, user.password)) {
           const token = await sign({ userId: user.id }, process.env.JWT_KEY);
+
+          // Sets lastEntered date to user
+          await prisma.updateUser({
+            data: {
+              lastEntered: new Date(),
+            },
+            where: {
+              id: user.id,
+            },
+          });
+
           return { token, user };
         }
         throw new AuthenticationError('Username or password incorrect');
       } catch (e) {
-        throw new AuthenticationError('Username or password incorrect');
+        throw new AuthenticationError(e);
       }
-    },
-    signUp(parent, { data }, { prisma }, info) {
-      const {
-        name, username, password, roleId,
-      } = data;
-      const user = {
-        name,
-        username,
-        password: hashSync(password, 7),
-        role: {
-          connect: {
-            id: roleId,
-          },
-        },
-      };
-      return prisma.createUser(user, info).$fragment(fragment);
     },
   },
 };
